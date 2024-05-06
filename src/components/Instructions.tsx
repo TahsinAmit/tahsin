557c8853038bff9ce706c6dd713ab50b23388b77
S M Tahsinur Rahman <Tahsinur.rahman@student.lut.fi>
2024-05-06 14:52:48 +0300

    Instructions3_updated

--- src/components/Instructions.tsx
+++ src/components/Instructions.tsx
@@ -155,37 +155,39 @@ const instruction = {
         <p>To start, we'll need some basic information:</p>
         <form onSubmit={handleSubmit(onSubmit)}>
           <br />
-          <label htmlFor="preferredDestination">Preferred Destination:</label>
-          <select id="preferredDestination" {...register('preferredDestination', { required: true })}>
-            <option value="">Select</option>
-            <option value="Malaga, Spain">Malaga, Spain</option>
-            <option value="Paris, France">Paris, France</option>
-            <option value="Milan, Italy">Milan, Italy</option>
+          <label htmlFor="learningGoal">Learning goal:</label><br />
+          <select id="learningGoal" {...register('learningGoal', { required: true })}>
+              <option value="">Select</option>
+              <option value="Gain new professional skill">Gain new professional skill</option>
+              <option value="Self improvement">Self improvement</option>
+              <option value="Preparation for certification">Preparation for certification</option>
           </select>
+          
           <br />
-          <label htmlFor="layoverDuration">Layover Duration:</label>
-          <select id="layoverDuration" {...register('layoverDuration', { required: true })}>
+          <label htmlFor="learningMethods">Preferred learning methods:</label><br />
+          <select id="learningMethods" {...register('learningMethods', { required: true })}>
             <option value="">Select</option>
-            <option value="None">None</option>
+            <option value="Video lectures">Video lectures</option>
+            <option value="Interactive">Interactive</option>
+            <option value="Lecture slides">Lecture slides</option>
+          </select>
+          
+          <br />
+          <label htmlFor="timePerWeek">Preferred time per week:</label><br />
+          <select id="timePerWeek" {...register('timePerWeek', { required: true })}>
+            <option value="">Select</option>
             <option value="2 hours">2 hours</option>
-            <option value="3 hours">3 hours</option>
+            <option value="5 hours">5 hours</option>
+            <option value="10 hours">10 hours</option>
           </select>
-
+          
           <br />
-          <label htmlFor="budget">Budget:</label>
-          <select id="budget" {...register('budget', { required: true })}>
+          <label htmlFor="fieldOfStudy">Preferred field of study:</label><br />
+          <select id="fieldOfStudy" {...register('fieldOfStudy', { required: true })}>
             <option value="">Select</option>
-            <option value="350 USD">350 USD</option>
-            <option value="450 USD">450 USD</option>
-            <option value="500 USD">500 USD</option>
-          </select>
-
-          <br />
-          <label htmlFor="seatPreference">Seat Preference:</label>
-          <select id="seatPreference" {...register('seatPreference', { required: true })}>
-            <option value="">Select</option>
-            <option value="Aisle">Aisle</option>
-            <option value="Window">Window</option>
+            <option value="Data Science">Data Science</option>
+            <option value="Marketing">Marketing</option>
+            <option value="Graphic design">Graphic design</option>
           </select>
           <br />
           <input type="submit" value="Click here to see AI generated recommendation"></input>
