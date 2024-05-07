d0d7b097bdbb3cc0630c747e5b92f7f627433e3c
Ruman <rumanbsl@gmail.com>
2024-05-07 15:09:19 +0300

    Session ID

--- src/App.tsx
+++ src/App.tsx
@@ -2,5 +2,8 @@ import { useAppState } from './state/state';
 
 export default function App() {
   const ActivePage = useAppState((s) => s.activePage);
-  return <ActivePage></ActivePage>;
+  const SessionID = useAppState((s) => s.sessionId);
+  return <div>
+    <h1>{SessionID}</h1>
+    <ActivePage></ActivePage></div>;
 }
