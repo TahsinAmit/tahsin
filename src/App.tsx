7e1af5155fc7b9b54d710b38ef261ee4cb2d9228
TahsinAmit <95619061+TahsinAmit@users.noreply.github.com>
2024-05-12 16:13:52 +0300

    csss

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useAppState } from './state/state';
-
+import "./App.css"
 export default function App() {
   const ActivePage = useAppState((s) => s.activePage);
   const SessionID = useAppState((s) => s.sessionId);
