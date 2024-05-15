1703765436c65874db335506868e6aed6bd3e654
Ruman <rumanbsl@gmail.com>
2024-05-15 18:04:47 +0300

    add cors

--- backend/index.ts
+++ backend/index.ts
@@ -1,8 +1,10 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const port = 8080;
 
+app.use(cors());
 app.use(express.json())
 
 app.post("/survey", (req, res) => {
